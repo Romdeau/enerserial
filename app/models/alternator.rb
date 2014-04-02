bb9708e5adba52bbe1902340be3072e5d7457499
Thomas Taege <thomas.taege@gmail.com>
2014-04-02 15:51:51 +1000

    can view unassigned alternators

--- app/models/alternator.rb
+++ app/models/alternator.rb
@@ -2,4 +2,7 @@ class Alternator < ActiveRecord::Base
   belongs_to :stock
 
   validate :serial, :alternator, presence: true
+
+  scope :floor_stock, -> { where stock_id: nil }
+	scope :assigned_stock, -> { where approved: !nil}
 end
