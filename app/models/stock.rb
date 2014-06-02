23e4bf57fb7766ce0ab06eb5cf7186b7730ed837
Thomas Taege <thomas.taege@gmail.com>
2014-06-02 11:58:15 +1000

    fixed faulty ppsr assignement, added assertion for serial number based off ppsr, hidden audit params for non-admins

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -25,18 +25,27 @@ class Stock < ActiveRecord::Base
 
   validate :serial_number, presence: true
   validate :valid_ppsr?
+  validate :valid_serial?
 
   def valid_job?
     if Job.find_by job_number: job_id != nil
       true
     else
-      errors.add(:job_id, "#{job_id} is not a valid job number")
+      errors.add(:job_id, "#{job_id} is not a valid job number.")
     end
   end
 
   def valid_ppsr?
     if status == "Ready to Ship" and needs_ppsr == true and (ppsr == nil or ppsr == '')
-      errors.add(:job_id, "#{ppsr} job cannot be ready to ship without a PPSR number")
+      errors.add(:job_id, "#{ppsr} job cannot be ready to ship without a PPSR number.")
+    else
+      true
+    end
+  end
+
+  def valid_serial?
+    if needs_ppsr == true and (ppsr == nil or ppsr == '') and (serial_number != nil and serial_number != '')
+      errors.add(:serial_number, "Cannot make Serial Number #{serial_number}. Job cannot be assigned a serial number without a valid PPSR.")
     else
       true
     end
